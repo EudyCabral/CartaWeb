df0d81c72796d791643e85cfff7416365ea6e67f
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2019-02-06 18:30:03 -0400

    Proyecto Creado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    
+        public class Contexto : DbContext
+        {
+
+           
+            public DbSet<CuentasBancarias> cuentasbancarias { get; set; }
+            public DbSet<Depositos> depositos { get; set; }
+
+
+
+        public Contexto() : base("ConStr")
+            {
+            }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    
+}
---
+++ Entidades/Cartas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cartas
+    {
+        [key]
+        public int Idcarta { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Destinatarioid { get; set; }
+        public string Cuerpo { get; set; }
+
+        public Cartas()
+        {
+            Idcarta = 0;
+            Fecha = DateTime.Now;
+            Destinatarioid = 0;
+            Cuerpo = string.Empty;
+        }
+    }
+}
