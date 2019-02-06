6a57787bd20ec3639850baccc5d0e15ce22607bd
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2019-02-06 18:32:15 -0400

    Entity Framework instalado

--- Entidades/Cartas.cs
+++ Entidades/Cartas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Entidades
 {
     public class Cartas
     {
-        [key]
+        [Key]
         public int Idcarta { get; set; }
         public DateTime Fecha { get; set; }
         public int Destinatarioid { get; set; }
